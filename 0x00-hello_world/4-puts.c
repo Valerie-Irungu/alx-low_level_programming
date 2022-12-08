ac9cb389bcc83717881dda81dcb8a964a57832c1
Valerie-Irungu <irunguvalerie@gmail.com>
2022-12-08 15:44:01 -0800

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
 # include <stdio.h>
 
 /* main - Entry point */
-int main (void)
+int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
