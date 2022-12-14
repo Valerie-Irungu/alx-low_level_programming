e9f74a904bb62ae6cd2deec75f4e4cb74df0bffd
Valerie-Irungu <irunguvalerie@gmail.com>
2022-12-13 17:01:54 -0800

    lowercase

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - prints the alphabet 10 times
+ */
+void print_alphabet__x10(void);
+{
+	int ten;
+	char low;
+
+	for (ten = 0; ten <= 9; ten++)
+	{
+		for (low = 'a'; la <= 'z'; low++)
+			_putchar(low);
+		_putchar('\n');
+	}
+}
