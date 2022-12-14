cf8c09bb8a6659e91ea168066aaa2b9ef2f7834a
Valerie-Irungu <irunguvalerie@gmail.com>
2022-12-14 02:29:25 -0800

    Print to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -7,16 +7,16 @@
  */
 void print_to_98(int n)
 {
-	if (n < 98)
+	if (n >= 98)
 	{
-		for (n = n; n < 98; n++)
-			_putchar("%d, ', n");
-		_putchar("%d\n", 98);
+		while (n > 98)
+			printf("%d, ", n--);
+		printf("%d\n", n);
 	}
 	else
 	{
-		for (n = n; n > 98; n--)
-			_putchar("%d, ", n);
-		_putchar("%d\n", 98);
+		while (n < 98)
+			printf("%d, ", n++);
+		printf("%d\n", n);
 	}
 }
