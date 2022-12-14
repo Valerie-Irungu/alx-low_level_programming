e934f7ff61a32ce82f2de8c3d95638313a8841c9
Valerie-Irungu <irunguvalerie@gmail.com>
2022-12-13 16:46:40 -0800

    lowercase

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * main - prints the alphabets in lowercase
+ */
+void print_alphabet(void)
+{
+	char low;
+
+	for (low = 'a'; low <= 'z'; low++)
+		_putchar(low);
+	_putchar('\n');
+}
