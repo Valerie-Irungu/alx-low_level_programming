fe11ea07596132636bec997ad0350374f2cd2af6
Valerie-Irungu <irunguvalerie@gmail.com>
2022-12-15 05:12:16 -0800

    isupper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -8,15 +8,15 @@
  */
 int _isupper(int c)
 {
-	char c;
+	char uppercase = 'A';
+	int isupper = 0;
 
-	c = 'A';
-
-	printf("%c: %d\n", c, _isupper(c));
-
-	c = 'a';
-
-	printf("%c: %d\n", c, _isupper(c));
-
-	return (0);
+	for (; uppercase <= 'z'; uppercase++)
+	{
+		if (c == uppercase)
+		{
+			isupper = 1;
+			break;
+		}
+	}
 }
