b8fc2f9fe3982d96719d161fa77707a3837ff7d5
Valerie-Irungu <irunguvalerie@gmail.com>
2023-01-05 06:08:04 -0800

    Recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _puts_recursion - prints a string with recursion
+ * @s: string to be printed
+ * Return: nothing
+ */
+void _puts_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_putchar(s[0]);
+		_puts_recursion(s + 1);
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
