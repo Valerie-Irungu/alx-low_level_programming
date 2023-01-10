005b6873ebf1e0de8dfcf12b95fa81430e3f4a2c
Valerie-Irungu <irunguvalerie@gmail.com>
2023-01-10 05:33:23 -0800

    my name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - prints the program name
+ * @argc: argurments in the program
+ * @argv: an array of pointers to the argurment
+ * Return: 0 on Success
+ */
+int main(int __attribute__((__unused__)) argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
