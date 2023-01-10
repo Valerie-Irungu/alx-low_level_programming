a0be4767fd87dbad88ccd8ab6a9fa521d710743d
Valerie-Irungu <irunguvalerie@gmail.com>
2023-01-10 09:57:22 -0800

    3.mul

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - prints the multiplication of two numbers
+ * @argc: the number of argurments
+ * @argv: array of pointers
+ * Return: product of two argurments
+ */
+int main(int argc, char *argv[])
+{
+	int num1, num2, prod;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
+	prod = num1 * num2;
+
+	print("%d\n", prod);
+	return (0);
+}
